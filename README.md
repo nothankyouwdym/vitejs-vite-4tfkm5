aefc92ffadc40581f7c4b18eac5b88d5b7b8cade
nothankyouwdym <boubacar.diallo2028@uppublicschools.org>
2022-06-29 13:15:10 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vitejs-vite-4tfkm5
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-4tfkm5)
---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+
+
+const App = () =>{
+  return(
+    <section>
+      <h1>Are you like me?</h1>
+    </section>
+  )
+}
+
+export default App
